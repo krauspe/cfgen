9264b7fe8b157c72ac103f12f935fd2f2f7edb34
kit02@peter-krauspe.de <kit02@peter-krauspe.de>
2016-03-07 16:42:24 +0000

    created more data structures : config_defaults, struct{} struct{} not used yet

--- bin/create_config.py
+++ bin/create_config.py
@@ -23,13 +23,14 @@ import argparse
 # tpl_type = args.type
 # tpl = args.tpl
 
-hn = 'etcd-01'
-#tpl_type = 'cloud-config'
+hn = 'etcd-02'
+tpl_type = 'cloud-config'
 #tpl_type = 'dhcpd'
-tpl_type = 'virt-install'
+#tpl_type = 'virt-install'
 #tpl = 'auto-install'
-tpl = 'xen'
-
+#tpl = 'xen'
+#tpl = 'entry'
+tpl = 'etcd'
 
 
 out_filename = {
@@ -38,8 +39,6 @@ out_filename = {
     'virt-install':'virt-install'+'.@@hn@@.sh',    
 }
 
-if tpl == 'auto-install':
-    out_filename['cloud-config'] = 'auto-install'+'.@@hn@@.yml'
 
 # print "hn = %s" % (hn)
 # print "tpl_type = %s" % (tpl_type)
@@ -50,7 +49,8 @@ basedir = os.path.dirname(pydir)
 confdir = os.path.join(basedir,"config")
 tpldir = os.path.join(basedir,"tpl")
 deploydir = os.path.join(basedir,"deployment")
-tpl_file = ''
+
+tpl_file = os.path.join(tpldir,tpl_type + '.' + tpl + '.tpl')
 
 
 # vmhost settings
@@ -73,17 +73,28 @@ host_defaults = {
     'bridge':'virbr2',
     'dn':'nw1.lgn.dfs.de',
     'ns':'192.169.42.10',
+    'is':'192.169.42.10',
     'gw':'192.169.42.1',
     'sn':'255.255.255.0',
+    'netconf-type':'dhcp',
     'cloud-config-server':'is01',
     'install-tpl':'xen'
+}
+
+config_defaults = {
+    'dhcpd':{
+        'subnet':'192.169.42.10',
+        'range_from':'192.169.42.100',
+        'range_to':'192.169.42.200',
+        'bc':'192.168.24.255'
+    }
 }
 
 hosts = {
     'etcd-01':{
         'vm-name':'etcd-01',
         'ip':'192.168.42.11',
-        'mac':'00:00:00:00:00:01',
+         'mac':'00:00:00:00:00:01',
         'type':'etcd',
         },
     'etcd-02':{
@@ -103,8 +114,44 @@ hosts = {
         'ip':'192.168.42.10',
         'mac':'00:16:3e:02:3d:c3',
         'type':'is',
+        'netconf-type':'static',
         }
 }
+
+struct = {
+    'cloud-config':{
+        'auto-install':{
+            'vars':['cloud-config-server','target-yml','disk'],
+            'generator':{
+                },
+            },
+        'etcd':{
+            'vars':['ip','hn','initial-cluster-string'],
+             'generator':{
+                 'initial-cluster-string':'getCoreosInitialClusterString',
+                },
+            },
+        'default-static-ip':{
+            'vars':['hn','dv','ip','hn','gw','ns'],
+             'generator':{
+                }
+        },
+        'default':{
+            'vars':['hn'],
+             'generators':{
+                },
+            }
+    },
+    'dhcpd':{
+        'conf':{
+            'vars':['dn','ns','subnet','range_from','range_to','bc','is','dhcpd-host-entrys'],
+             'generator':{
+                 'dhcpd-host-entrys':'getAllDhcpHostEntrys',
+                },
+            },
+        }
+}
+
 
 def get_ip(hn):
     ip = ''
@@ -115,44 +162,58 @@ def get_ip(hn):
     return ip
 
 
-def get_coreos_initial_cluster_string(*args):
+def getCoreosInitialClusterString():
     string = ''
-    for hn in args:
-        ip = hosts[hn]['ip']
-        string += hn+"=http://"+ip+":2380,"
+    for hn in hosts.iterkeys():
+        if hosts[hn]['type'] == 'etcd':
+            ip = hosts[hn]['ip']
+            string += hn+"=http://"+ip+":2380,"
     string = string.rstrip(',')
     return string
-    
 
-settings = host_defaults.copy()
-settings['hn'] = hn
-settings.update(hosts[hn])
+def getAllDhcpHostEntrys():
+    hostlist = []
+    for hn in hosts.iterkeys():
+        if hosts[hn]['netconf-type'] == 'dhcp':
+            hostlist.append()
 
-settings['target-yml'] = hn+'.yml'
-settings['img-path'] = os.path.join(img_basedir,settings['vm-name'],settings['img-name'])
-out_filename[tpl_type] = out_filename[tpl_type].replace('@@hn@@',hn)
+#TODO: createObject muesste eigentlich als params noch tpl oder / und tpl_type mitbekomen und nur die relevanten settings erzeugen
+#TODO: "getCoreosInitialClusterString" und "getAllDhcpHostEntrys" anwenden um 'initial-cluster-string' und 'dhcpd-host-entrys' zu erzeugen ,oder
+#TODO: globalgalaktischgenerischen Ansatz entwickeln :-)
 
-tpl_file = os.path.join(tpldir,tpl_type + '.' + tpl + '.tpl')
-out_file = os.path.join(deploydir,out_filename[tpl_type])
+def createObject(hn):
+    settings['hn'] = hn
+    settings.update(hosts[hn])
 
-infile = open(tpl_file,"r")
-out_list = []
+    settings['target-yml'] = hn+'.yml'
+    settings['img-path'] = os.path.join(img_basedir,settings['vm-name'],settings['img-name'])
 
-for line in infile:
+    with open(tpl_file,'r+') as f:
+        contens = f.read()
+
     for seStr,repStr in settings.iteritems():
         if repStr != '':
-            l = line.replace('@@'+seStr+'@@',repStr)
-            line = l
-    print line
-    out_list.append(line)
+            contens = contens.replace('@@'+seStr+'@@',repStr)
+    return contens
 
-infile.close()
+#TODO: filenem/(path ?) generation in eigene Struktur mit unterscheidung ob host-bezogen (yml Files) oder liste (dhcpd.con) etc
+#TODO ggfs hn aus writeFile hearusnehmen und abghaengig vom tpl/tpl-type anderen parameter uebergeben ?!
+
+def writeFile(hn,contens):
+    if tpl == 'auto-install':
+        out_filename['cloud-config'] = 'auto-install'+'.@@hn@@.yml'
+    filename = out_filename[tpl_type].replace('@@hn@@',hn)
+    out_file = os.path.join(deploydir,filename)
+
+    print "create " + out_file
+    with open(out_file,'w+') as f:
+        f.write(contens)
 
 
+# main
 
-print "create " + out_file
+settings = host_defaults.copy()
+contens = createObject(hn)
 
-outfile = open(out_file, "w")
-for line in out_list:
-    outfile.write(line)
-outfile.close()
+writeFile(hn,contens)
+
